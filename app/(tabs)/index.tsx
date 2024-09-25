ae0773df477b2ef8c7f07a28c939b4bc937996e3
Deivid de Jesus <imdeividsilva@gmail.com>
2024-09-25 11:40:57 -0300

    criação do Services

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,27 +4,45 @@ import { DocsContext } from '@/context/DocsContext'
 import Container from '@/components/common/Container'
 import RevenueTitle from '@/components/info/RevenueTitle'
 import RevenueChart from '@/components/info/RevenueChart'
-import { StyleSheet, Text, View } from 'react-native'
+import { ScrollView, View } from 'react-native'
 import AnyItemWarning from '@/components/common/AnyItemWarning'
 import RevenueList from '@/components/info/RevenueList'
 import MonthInput from '@/components/common/MonthInput'
+import MostOfferedServicesChart from '@/components/info/MostOfferedServicesChart'
+import MostOfferedServicesTitle from '@/components/info/MostOfferedServicesTitle'
 
 export default function Info() {
 
     const [schedulings] = useContext(DocsContext).schedulings
 
+    const Revenue = () => (
+        <View>
+            <RevenueTitle />
+            <RevenueChart />
+            <RevenueList />
+        </View>
+    )
+
+    const Services = () => (
+        <View>
+            <MostOfferedServicesTitle />
+            <MostOfferedServicesChart />
+        </View>
+    )
+
     return (
         <Container>
-            <MonthInput />
+            <ScrollView>
             {
                 schedulings[0]
                     ? <View>
-                        <RevenueTitle />
-                        <RevenueChart />
-                        <RevenueList />
+                        <MonthInput />
+                        <Revenue />
+                        <Services />
                     </View>
                     : <AnyItemWarning text='Nenhuma informação disponível' />
             }
+            </ScrollView>
         </Container>
     )
 
