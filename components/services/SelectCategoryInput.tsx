21a7b9cf9d1547fc98426a828433998304a4b5ec
Deivid de Jesus <imdeividsilva@gmail.com>
2024-10-15 15:16:43 -0300

    so mostrando categorias possíveis

--- components/services/SelectCategoryInput.tsx
+++ components/services/SelectCategoryInput.tsx
@@ -1,4 +1,6 @@
+import { DocsContext } from '@/context/DocsContext'
 import { Picker } from '@react-native-picker/picker'
+import { useContext } from 'react'
 
 interface SelectCategoryInputProps {
     category: string
@@ -7,22 +9,37 @@ interface SelectCategoryInputProps {
 
 export default function SelectCategoryInput({ category, setCategory }: SelectCategoryInputProps) {
 
+    const [services] = useContext(DocsContext).services
+
     const categories = [
         ['Produtos', 'product'],
         ['Serviços', 'service'],
         ['Orçamentários', 'budget']
     ]
 
+    const thereIsItem = (category: string) => {
+
+        const item = services.find(item => (
+            item.category === category
+        ))
+
+        if (item) return true
+        return false
+
+    }
+
     return (
         <Picker
             selectedValue={category}
             onValueChange={itemValue => setCategory(itemValue)}
         >
-            {categories.map((current, index) => {
-                return (
-                    <Picker.Item key={current[0]} label={current[0]} value={current[1]} />
-                )
-            })}
+            {
+                categories.map((current) => (
+                    thereIsItem(current[1]) && (
+                        <Picker.Item key={current[0]} label={current[0]} value={current[1]} />
+                    )
+                ))
+            }
         </Picker>
     )
 
