a2e10584c717c03a0c881c723c0501e41131729c
Deivid de Jesus <imdeividsilva@gmail.com>
2024-12-15 12:49:20 -0300

    unnecessary files

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,11 +1,9 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native'
 import { useFonts } from 'expo-font'
 import { Stack } from 'expo-router'
 import * as SplashScreen from 'expo-splash-screen'
 import { useEffect } from 'react'
 import 'react-native-reanimated'
 
-import { useColorScheme } from '@/hooks/useColorScheme'
 import DocsProvider from '@/context/DocsContext'
 import HideTabBarProvider from '@/context/MainDisplays'
 import ContentProvider from '@/context/InfoContent'
@@ -15,7 +13,6 @@ import { setStatusBarBackgroundColor, setStatusBarStyle } from 'expo-status-bar'
 SplashScreen.preventAutoHideAsync()
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme()
   const [loaded] = useFonts({
     SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
   })
@@ -36,12 +33,9 @@ export default function RootLayout() {
     <HideTabBarProvider>
       <DocsProvider>
         <ContentProvider>
-          <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
             <Stack>
               <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-              <Stack.Screen name="+not-found" />
             </Stack>
-          </ThemeProvider>
         </ContentProvider>
       </DocsProvider>
     </HideTabBarProvider>
