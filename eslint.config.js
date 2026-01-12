998e2ae4cd1f6bd8f8b644717665566d36e38bb4
Deivid de Jesus <imdeividsilva@gmail.com>
2026-01-12 12:03:32 -0300

    eslint

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+const { defineConfig } = require('eslint/config')
+const expoConfig = require('eslint-config-expo/flat')
+
+module.exports = defineConfig([
+  expoConfig,
+  {
+    files: ['**/*.js', '**/*.jsx', '**/*.ts', '**/*.tsx'],
+    rules: {
+      // Força o uso de aspas simples
+      'quotes': ['error', 'single'],
+      // Desativa a obrigatoriedade do ponto e vírgula
+      'semi': ['error', 'never'],
+    },
+  },
+  {
+    ignores: ['dist/*'],
+  }
+])
