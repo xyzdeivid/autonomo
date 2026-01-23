40bb6a6933da391f15f09a67843d31b5d3b7300a
Deivid de Jesus <imdeividsilva@gmail.com>
2026-01-23 20:18:17 -0300

    exclusao de libs não usadas

--- components/index/YearOptions.tsx
+++ components/index/YearOptions.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
-import YearPicker from './YearOptions/YearPicker'
-import OpenPickerButton from './YearOptions/OpenPickerButton'
+import YearPicker from './YearPicker'
+import OpenPickerButton from './OpenPickerButton'
 
 interface YearOptionsProps {
     availableYears: string[]
