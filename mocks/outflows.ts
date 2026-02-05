71abf04480002acddd5d50dabc8d5c9303640220
Deivid de Jesus <imdeividsilva@gmail.com>
2026-02-05 17:39:56 -0300

    criação de mock de outflows

---
+++ mocks/outflows.ts
@@ -0,0 +1,14 @@
+import { Outflow } from '@/types'
+
+export const outflows: Outflow[] = [
+    { _id: '1', name: 'Aluguel', date: '2026-01-05', value: 400 },
+    { _id: '2', name: 'Internet', date: '2026-01-06', value: 80 },
+    { _id: '3', name: 'Energia elétrica', date: '2026-01-08', value: 120 },
+    { _id: '4', name: 'Água', date: '2026-01-09', value: 60 },
+    { _id: '5', name: 'Supermercado', date: '2026-01-12', value: 150 },
+    { _id: '6', name: 'Transporte', date: '2026-01-15', value: 50 },
+    { _id: '7', name: 'Streaming', date: '2026-01-18', value: 30 },
+    { _id: '8', name: 'Farmácia', date: '2026-01-20', value: 40 },
+    { _id: '9', name: 'Lanche', date: '2026-01-22', value: 25 },
+    { _id: '10', name: 'Manutenção', date: '2026-01-27', value: 35 },
+]
